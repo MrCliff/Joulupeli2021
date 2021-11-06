73dccbaadff59a94cc2cf5ba4d58c717d020bce8
MrCliff <j_1.kallio@hotmail.com>
2021-11-06 23:13:03 +0200

    Initialize project
    * Copy (and edit) .gitignore, LICENSE, README.md and deploy-game.sh
      from last year's project.
    * Add new Unity project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Joulupeli 2021
+
+Joulupeli 2021 on ... (TODO)
+
+Peliä voi pelata osoitteessa [kallio.netlify.app](https://kallio.netlify.app/).
+
+Peli on toteutettu käyttäen Unity-pelimoottoria.
---
+++ deploy-game.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ./Joulupeli-2021/Builds/WebGL/Build/* ../Joulupelinettisivu/public/Joulupeli2021/
+cp ./Joulupeli-2021/Builds/WebGL/TemplateData/* ../Joulupelinettisivu/public/TemplateData/
+cp ./Joulupeli-2021/Builds/Joulupeli2021.apk ../Joulupelinettisivu/public/Joulupeli2021/Joulupeli2021.apk
