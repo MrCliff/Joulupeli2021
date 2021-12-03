ae39e0fb75504ebb8455ce328dc846d771730ef4
MrCliff <j_1.kallio@hotmail.com>
2021-12-03 23:49:57 +0200

    Tweak spawn rate and automate build version
    Build version is now incremented automatically on build. Add also
    keystore for Android build.

--- Joulupeli-2021/Assets/Editor/Build/BuildProject.cs
+++ Joulupeli-2021/Assets/Editor/Build/BuildProject.cs
@@ -2,17 +2,26 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEditor.Build.Reporting;
+using System.IO;
+using System;
 
 namespace Assets.Editor.Build
 {
     public class BuildProject
     {
+        private const string ENV_VAR_KEYSTORE_PATH = "UNITY_KEYSTORE_PATH";
+        private const string ENV_VAR_KEYSTORE_MASTER_PASS = "UNITY_KEYSTORE_MASTER_PASSWORD";
+        private const string ENV_VAR_KEY_ALIAS = "UNITY_KEY_ALIAS";
+        private const string ENV_VAR_KEY_PASS = "UNITY_KEY_PASSWORD";
 
+        [MenuItem("Build/Build all")]
         public static int BuildAll()
         {
+            IncrementBundleVersion();
+
             int returnCode;
-            returnCode = BuildAndroid();
-            int returnCodeWebGL = BuildWebGL();
+            returnCode = BuildAndroid(false);
+            int returnCodeWebGL = BuildWebGL(false);
             if (returnCode == 0)
             {
                 returnCode = returnCodeWebGL;
@@ -20,9 +29,19 @@ namespace Assets.Editor.Build
             
             return returnCode;
         }
-        
+
+        [MenuItem("Build/Build WebGL")]
         public static int BuildWebGL()
         {
+            return BuildWebGL(true);
+        }
+
+        public static int BuildWebGL(bool incrementVersion)
+        {
+            if (incrementVersion)
+            {
+                IncrementBundleVersion();
+            }
             Debug.Log("Start build for WebGL...");
             BuildReport report = PerformBuild("Builds/WebGL/", BuildTarget.WebGL);
             if (report.summary.result == BuildResult.Failed)
@@ -34,9 +53,24 @@ namespace Assets.Editor.Build
             return 0;
         }
 
+        [MenuItem("Build/Build Android")]
         public static int BuildAndroid()
         {
+            return BuildAndroid(true);
+        }
+
+        public static int BuildAndroid(bool incrementVersion)
+        {
+            if (incrementVersion)
+            {
+                IncrementBundleVersion();
+            }
             Debug.Log("Start build for Android...");
+
+            PlayerSettings.Android.keystoreName = Environment.GetEnvironmentVariable(ENV_VAR_KEYSTORE_PATH);
+            PlayerSettings.Android.keystorePass = Environment.GetEnvironmentVariable(ENV_VAR_KEYSTORE_MASTER_PASS);
+            PlayerSettings.Android.keyaliasName = Environment.GetEnvironmentVariable(ENV_VAR_KEY_ALIAS);
+            PlayerSettings.Android.keyaliasPass = Environment.GetEnvironmentVariable(ENV_VAR_KEY_PASS);
             BuildReport report = PerformBuild("Builds/Joulupeli2021.apk", BuildTarget.Android);
             if (report.summary.result == BuildResult.Failed)
             {
@@ -55,7 +89,30 @@ namespace Assets.Editor.Build
             };
             options.locationPathName = outPath;
             options.target = target;
+
             return BuildPipeline.BuildPlayer(options);
+        }
+
+        private static void IncrementBundleVersion()
+        {
+            string versionText = File.ReadAllText("version.txt");
+
+            if (versionText != null)
+            {
+                string[] lines = versionText.Trim().Split('.');
+
+                int majorVersion = int.Parse(lines[0]);
+                int minorVersion = int.Parse(lines[1]);
+                int subMinorVersion = int.Parse(lines[2]) + 1;
+
+                versionText = majorVersion.ToString() + "." +
+                    minorVersion.ToString() + "." +
+                    subMinorVersion.ToString();
+
+                PlayerSettings.bundleVersion = versionText;
+                PlayerSettings.Android.bundleVersionCode = majorVersion*10000 + minorVersion*100 + subMinorVersion;
+                File.WriteAllText("version.txt", versionText);
+            }
         }
     }
 }
--- deploy-game.sh
+++ deploy-game.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
-unity.bat -quit -batchmode -projectPath ./Joulupeli-2021 -executeMethod Assets.Editor.Build.BuildProject.BuildAll
+PROJECT_PATH="./Joulupeli-2021"
+BUILD_PATH="${PROJECT_PATH}/Builds"
+LOG_PATH="${PROJECT_PATH}/Logs"
+WEBSITE_PATH="../Joulupelinettisivu/public/Joulupeli2021"
 
-cp ./Joulupeli-2021/Builds/WebGL/Build/* ../Joulupelinettisivu/public/Joulupeli2021/
-cp ./Joulupeli-2021/Builds/Joulupeli2021.apk ../Joulupelinettisivu/public/Joulupeli2021/Joulupeli2021.apk
+if [ ! "${UNITY_KEYSTORE_PATH}" ];then
+    echo "Environment variable UNITY_KEYSTORE_PATH has no value."
+    exit 1
+elif [ ! "${UNITY_KEYSTORE_MASTER_PASSWORD}" ];then
+    echo "Environment variable UNITY_KEYSTORE_MASTER_PASSWORD has no value."
+    exit 1
+elif [ ! "${UNITY_KEY_ALIAS}" ];then
+    echo "Environment variable UNITY_KEY_ALIAS has no value."
+    exit 1
+elif [ ! "${UNITY_KEY_PASSWORD}" ];then
+    echo "Environment variable UNITY_KEY_PASSWORD has no value."
+    exit 1
+fi
+
+echo "Start building for all targets..."
+unity.sh -quit -batchmode -projectPath "${PROJECT_PATH}" -executeMethod Assets.Editor.Build.BuildProject.BuildAll -logfile "${LOG_PATH}/build.log"
+echo "Build done!"
+
+echo "Removing old files from website path '${WEBSITE_PATH}/'..."
+rm "${WEBSITE_PATH}/"*
+echo "Copying WebGL build to the website path..."
+cp "${BUILD_PATH}/WebGL/Build/"* "${WEBSITE_PATH}/"
+echo "Copying Android build to the website path..."
+cp "${BUILD_PATH}/Joulupeli2021.apk" "${WEBSITE_PATH}/Joulupeli2021.apk"
+echo "Copying version number to the website path..."
+cp "${PROJECT_PATH}/version.txt" "${WEBSITE_PATH}/version.txt"
+
+echo "Done!"
